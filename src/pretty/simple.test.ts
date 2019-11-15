859c81ea364225d7a9937949b33904ae3d4ff61b
Blayne Chard <blayne@chard.com>
2019-11-16 12:06:03 +1300

    feat: adding simple pretty tests

---
+++ src/pretty/simple.test.ts
@@ -0,0 +1,16 @@
+import * as o from 'ospec';
+import { PrettySimple } from './simple';
+
+o.spec('Simple', () => {
+    o('should return null when below log level', () => {
+        const simple = new PrettySimple(50);
+        o(simple.pretty({ level: 49, time: 1, msg: 'abc' })).equals(null);
+        o(simple.pretty({ level: 50, time: 1, msg: 'abc' })).notEquals(null);
+        o(simple.pretty({ level: 51, time: 1, msg: 'abc' })).notEquals(null);
+    });
+
+    o('should not pretty if time is not valid', () => {
+        const simple = new PrettySimple(50);
+        o(simple.pretty({ level: 50, time: 'abc', msg: 'abc' })).equals(null);
+    });
+});
